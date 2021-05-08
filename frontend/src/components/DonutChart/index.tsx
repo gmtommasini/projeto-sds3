f0fb3d0a8bd0a5e7bb58049a59b9b425896d396a
Guilherme Tommasini <gmtommasini@gmail.com>
2021-05-08 10:19:09 -0400

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { saleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,36 +11,45 @@ type ChartData = {
 
 const DonutChart = () => {
 
+    //state
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    //setChartData updates the value of chartData
     //wrong way
-    let chartData: ChartData = {labels:[], series: []};
+    //let chartData: ChartData = {labels:[], series: []};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then( (response) => {
-            const data = response.data as saleSum[];
-            const myLabels =  data.map( x => x.sellerName);
-            const mySeries = data.map( x => x.sum);
+    useEffect(() => {
+        //must be called just once, that's why it's inside the useEffect
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                const data = response.data as saleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-
-        }  )
+                //chartData = {labels: myLabels, series: mySeries};
+                setChartData({ labels: myLabels, series: mySeries });
+                //console.log(chartData);
+            })
+    },
+        //list of objects that useEffect will monitor
+        []);
 
     // const mockData = {
-        //     series: [477138, 499928, 444867, 220426, 473088],
-        //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        // }
-        
-        const options = {
-            legend: {
-                show: true
-            }
+    //     series: [477138, 499928, 444867, 220426, 473088],
+    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    // }
+
+    const options = {
+        legend: {
+            show: true
         }
-        
-        // from apexcharts.com examples for React
+    }
+
+    // from apexcharts.com examples for React
+    //even with update data, chart is not built. Need to use state
     return (
-        <Chart  
-            options={{ ...options, labels : chartData.labels}}
-            series= {chartData.series}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height='240'
         />
