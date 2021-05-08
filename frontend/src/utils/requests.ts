3041fe8a39ac1a5832ff5d284fe074e141c97c96
Guilherme Tommasini <gmtommasini@gmail.com>
2021-05-08 12:26:58 -0400

    env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+// "??" works as || in JS, if the first is null, will get the second
